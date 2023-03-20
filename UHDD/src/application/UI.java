cc912298fd06e1b9fdaa938d79bfd02f77b5d52b
FPStruck <mina.gemian79@gmail.com>
2023-03-20 17:11:46 +1100

    First trial iteration of UI
    Contains interaction between Main, UI and Login. Submit button interaction has been implemented to say "Hello user"

--- UHDD/src/application/UI.java
+++ UHDD/src/application/UI.java
@@ -1,27 +1,35 @@
 package application;
 	
 import javafx.stage.Stage;
+
+import java.io.IOException;
+
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.text.Text;
 
 
-public class UI extends Application {
-	@Override
-	
-	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
+public class UI {
+	private Stage stage;
+	private Scene scene;
+	private Parent root;
+	@FXML private Text actionGrabber;
+		
+	public void switchToCreateUser(ActionEvent event) throws IOException {
+		Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
+		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
 	}
 	
-	public static void main(String[] args) {
-		launch(args);
+	@FXML protected void handleSubmitAction(ActionEvent event) {
+		actionGrabber.setText("Hello user");
 	}
 }
