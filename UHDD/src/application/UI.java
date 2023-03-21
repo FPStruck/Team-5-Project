3224126d808795649f657011ad32576a7634e794
FPStruck <mina.gemian79@gmail.com>
2023-03-21 21:16:35 +1100

    Development
    Added new page, buttons and interaction

--- UHDD/src/application/UI.java
+++ UHDD/src/application/UI.java
@@ -11,7 +11,9 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 
 
@@ -20,8 +22,22 @@ public class UI {
 	private Scene scene;
 	private Parent root;
 	@FXML private Text actionGrabber;
+	@FXML private TextField userGrabber;
+	@FXML private TextField passGrabber;
+	
+	@FXML private Text actionGrabberCreator;
+	@FXML private TextField userGrabberCreator;
+	@FXML private TextField passGrabberCreator;
 		
 	public void switchToCreateUser(ActionEvent event) throws IOException {
+		Parent root = FXMLLoader.load(getClass().getResource("UserCreation.fxml"));
+		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
+	}
+	
+	public void switchToHomepage(ActionEvent event) throws IOException {
 		Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
 		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
 		scene = new Scene(root);
@@ -29,7 +45,43 @@ public class UI {
 		stage.show();
 	}
 	
-	@FXML protected void handleSubmitAction(ActionEvent event) {
-		actionGrabber.setText("Hello user");
+	public void loginSuccessful() {
+		actionGrabber.setText("Login Successful");
+		actionGrabber.setFill(Color.GREEN);
+	}
+	
+	@FXML protected void handleSignInAction(ActionEvent event) {
+		
+		if(userGrabber.getText().equals("") & passGrabber.getText().equals("")) {
+			actionGrabber.setText("Username and Password cannot be empty");
+			actionGrabber.setFill(Color.RED);
+		} else if(userGrabber.getText().equals("")) {
+			actionGrabber.setText("Username cannot be empty");
+			actionGrabber.setFill(Color.RED);
+		} else if(passGrabber.getText().equals("")) {
+			actionGrabber.setText("Password cannot be empty");
+			actionGrabber.setFill(Color.RED);
+		} 
+		else {
+			loginSuccessful();
+		}
+	}
+	
+@FXML protected void handleCreateNewUsernAction(ActionEvent event) {
+		
+		if(userGrabberCreator.getText().equals("") & passGrabberCreator.getText().equals("")) {
+			actionGrabberCreator.setText("Username and Password cannot be empty");
+			actionGrabberCreator.setFill(Color.RED);
+		} else if(userGrabberCreator.getText().equals("")) {
+			actionGrabberCreator.setText("Username cannot be empty");
+			actionGrabberCreator.setFill(Color.RED);
+		} else if(passGrabberCreator.getText().equals("")) {
+			actionGrabberCreator.setText("Password cannot be empty");
+			actionGrabberCreator.setFill(Color.RED);
+		} 
+		else {
+			actionGrabberCreator.setText("User Creation Successful");
+			actionGrabberCreator.setFill(Color.GREEN);
+		}
 	}
 }
