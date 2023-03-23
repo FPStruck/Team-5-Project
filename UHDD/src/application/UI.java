98036f99af9bc16fc1b655434febb188ccac5c08
FPStruck <mina.gemian79@gmail.com>
2023-03-23 16:59:22 +1100

    Development on UI
    Added a gridpane to fit textfields that can be added in the "records" scene using a single button. Iteration of new fields using the button is yet to be developed

--- UHDD/src/application/UI.java
+++ UHDD/src/application/UI.java
@@ -3,16 +3,22 @@ package application;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 
@@ -25,6 +31,8 @@ public class UI {
 	@FXML private TextField userGrabber;
 	@FXML private TextField passGrabber;
 	
+	@FXML private GridPane maingrid;
+	@FXML private AnchorPane anchor;
 	@FXML private Text actionGrabberCreator;
 	@FXML private TextField userGrabberCreator;
 	@FXML private TextField passGrabberCreator;
@@ -45,12 +53,21 @@ public class UI {
 		stage.show();
 	}
 	
-	public void loginSuccessful() {
-		actionGrabber.setText("Login Successful");
-		actionGrabber.setFill(Color.GREEN);
+	public void switchToTableCreator(ActionEvent event) throws IOException {
+		Parent root = FXMLLoader.load(getClass().getResource("TableCreator.fxml"));
+		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
 	}
 	
-	@FXML protected void handleSignInAction(ActionEvent event) {
+	public boolean loginSuccessful() {
+		actionGrabber.setText("Login Successful");
+		actionGrabber.setFill(Color.GREEN);
+		return true;
+	}
+	
+	@FXML protected void handleSignInAction(ActionEvent event) throws IOException {
 		
 		if(userGrabber.getText().equals("") & passGrabber.getText().equals("")) {
 			actionGrabber.setText("Username and Password cannot be empty");
@@ -63,7 +80,13 @@ public class UI {
 			actionGrabber.setFill(Color.RED);
 		} 
 		else {
-			loginSuccessful();
+			if(loginSuccessful() == true) {
+				Parent root = FXMLLoader.load(getClass().getResource("TableCreator.fxml"));
+				stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+				scene = new Scene(root);
+				stage.setScene(scene);
+				stage.show();
+			}
 		}
 	}
 	
@@ -83,5 +106,13 @@ public class UI {
 			actionGrabberCreator.setText("User Creation Successful");
 			actionGrabberCreator.setFill(Color.GREEN);
 		}
+	}
+	
+	@FXML protected void handleAddButton(ActionEvent event) {
+			int column = 0;
+			for(int row = 0; row < 4; row++) {
+				maingrid.add(new TextField(), row, column);
+			}
+			
 	}
 }
