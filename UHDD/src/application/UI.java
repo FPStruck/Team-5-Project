cf4c85a4b971e24913748f73c746aab1eb5a3c17
TitanSri <104621169+TitanSri@users.noreply.github.com>
2023-04-16 19:30:13 +1000

    Create FXML MySQL
    Converted the current MySQL to FXML for the ability to use scene builder to build the UI

--- UHDD/src/application/UI.java
+++ UHDD/src/application/UI.java
@@ -8,6 +8,11 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.file.Path;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -25,6 +30,7 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TablePosition;
@@ -58,12 +64,42 @@ public class UI {
 	@FXML private TextField passGrabberCreator;
 	@FXML private Button addColumn;
 	@FXML private Button addRow;
+	@FXML private Button view;
+	@FXML private Button update;
+	@FXML private Button insert;
+	@FXML private Button clear;
 	
 	//Toby's changes
 	static byte[] encryptedPassword; // for the encryption
 	static byte[] decryptedPassword; // for the decryption
 	static RSA_DSA rsa;
 	static byte[] tempByteArray;
+	
+	@FXML private Label labelStatus = new Label();
+	
+	@FXML private Label labelId = new Label("ID");
+	@FXML private TextField textId = new TextField();
+	@FXML private Label labelLastName = new Label("Last Name");
+	@FXML private TextField textLastName = new TextField();
+	@FXML private Label labelFirstName = new Label("First Name");
+	@FXML private TextField textFirstName = new TextField();
+	@FXML private Label labelMiddleName = new Label("Middle Name");
+	@FXML private TextField textMiddleName = new TextField();
+	@FXML private Label labelAddress = new Label("Address");
+	@FXML private TextField textAddress = new TextField();
+	@FXML private Label labelCity = new Label("City");
+	@FXML private TextField textCity = new TextField();
+	@FXML private Label labelState = new Label("State");
+	@FXML private TextField textState = new TextField();
+	@FXML private Label labelTelephone = new Label("Telephone");
+	@FXML private TextField textTelephone = new TextField();
+	@FXML private Label labelEmail = new Label("Email");
+	@FXML private TextField textEmail = new TextField();
+	
+	// these objects will be used in querying the database and processing the results
+	private Connection connection;
+	private Statement statement;
+	private ResultSet results;
 
 	public void switchToCreateUser(ActionEvent event) throws IOException {
 		Parent root = FXMLLoader.load(getClass().getResource("UserCreation.fxml"));
@@ -90,13 +126,23 @@ public class UI {
 	}
 	
 	public void switchToMySQL(ActionEvent event) throws Exception {
-		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
 		Parent root = FXMLLoader.load(getClass().getResource("Test.fxml"));
+		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
 		scene = new Scene(root);
 		stage.setScene(scene);
 		stage.show();
 		MySQL_test mySQL_test = new MySQL_test();
 		mySQL_test.start(stage);
+	}
+	
+	public void switchToMySQLTest(ActionEvent event) throws Exception {
+		Parent root = FXMLLoader.load(getClass().getResource("MySQL.fxml"));
+		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
+//		MySQL_test2 mySQL_test2 = new MySQL_test2();
+//		mySQL_test2.start(stage);
 	}
 	
 	public boolean loginSuccessful() {
@@ -355,6 +401,136 @@ public class UI {
 			e.printStackTrace();
 		}
 		return false;
+	}
+	
+	@FXML public void view(ActionEvent event) throws ClassNotFoundException, SQLException {
+		System.out.println("viewed");
+		// TODO Auto-generated method stub
+		initialDB();
+		String query = "SELECT * FROM `javabook`.`test` WHERE ID = '" + textId.getText().trim() + "'";
+		
+		try {
+			// execute statement
+			results = statement.executeQuery(query);
+			loadFields(results);
+			
+		} catch (SQLException Ex){
+			labelStatus.setText("Record failed:");
+			System.out.println(Ex.getMessage());
+		}
+	}
+	
+	@FXML private void update(ActionEvent event) {
+		// TODO Auto-generated method stub
+		String updateQuery = "UPDATE `javabook`.`test` SET "
+				+ "FirstName = '" + textFirstName.getText().trim() + 
+				"', MiddleName = '" + textMiddleName.getText().trim()+ 
+				"', LastName = '" + textLastName.getText().trim() + 
+				"', Address = '" + textAddress.getText().trim() + 
+				"', City = '" + textCity.getText().trim() +
+				"', State = '" + textState.getText().trim() + 
+				"', Telephone = '" + textTelephone.getText().trim() + 
+				"', Email = '" + textEmail.getText().trim() + 
+				"' WHERE ID = '" + textId.getText().trim() + "';";
+		
+		System.out.println(updateQuery);
+		
+		try {
+			// execute statement
+			statement.executeUpdate(updateQuery);
+			labelStatus.setText("Update completed:");
+			System.out.println("Update suceeded");
+			
+		} catch (SQLException Ex){
+			labelStatus.setText("Update failed:");
+			System.out.println(Ex.getMessage());
+		}
+	}
+	
+	@FXML private void clear(ActionEvent event) {
+		// TODO Auto-generated method stub
+		textId.setText(null);
+		textLastName.setText(null);
+		textFirstName.setText(null);
+		textMiddleName.setText(null);
+		textAddress.setText(null);
+		textCity.setText(null);
+		textState.setText(null);
+		textTelephone.setText(null);
+		textEmail.setText(null);
+	}
+	
+	@FXML private void insert(ActionEvent event) {
+		// TODO Auto-generated method stub
+		String insertQuery = "INSERT INTO `javabook`.`test` "
+				+ "(ID, FirstName, MiddleName, LastName, Address, City, State, Telephone, Email) "
+				+ "VALUES ('" + textId.getText().trim() + "', '" + textFirstName.getText().trim() + 
+				"', '" + textMiddleName.getText().trim()+ "', '" + textLastName.getText().trim() + 
+				"', '" + textAddress.getText().trim() + "', '" + textCity.getText().trim() +
+				"', '" + textState.getText().trim() + "', '" + textTelephone.getText().trim() + 
+				"', '" + textEmail.getText().trim() + "');";
+		
+		//System.out.println(insertQuery);
+		
+		try {
+			// execute statement
+			statement.executeUpdate(insertQuery);
+			labelStatus.setText("Insert completed:");
+			System.out.println("Insert suceeded");
+			
+		} catch (SQLException Ex){
+			labelStatus.setText("Insert failed:");
+			System.out.println(Ex.getMessage());
+		}
+		
+	}
+	
+	private void loadFields(ResultSet results) throws SQLException {
+		// TODO Auto-generated method stub
+		if (results.next()) {
+			textLastName.setText(results.getString(2));
+			textFirstName.setText(results.getString(3));
+			textMiddleName.setText(results.getString(4));
+			textAddress.setText(results.getString(5));
+			textCity.setText(results.getString(6));
+			textState.setText(results.getString(7));
+			textTelephone.setText(results.getString(8));
+			textEmail.setText(results.getString(9));
+			labelStatus.setText("Record found");
+		} else {
+			textLastName.setText("");
+			textFirstName.setText("");
+			textMiddleName.setText("");
+			textAddress.setText("");
+			textCity.setText("");
+			textState.setText("");
+			textTelephone.setText("");
+			textEmail.setText("");
+			labelStatus.setText("Record not found");
+		}
+	}
+	
+	private void initialDB() throws ClassNotFoundException, SQLException {
+		// TODO Auto-generated method stub
+		// loads and checks the driver
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		System.out.println("Driver loaded:");
+		
+		// connection for database...make sure the URL is correct JDBC:MYSQL
+		String url = "jdbc:mysql://127.0.0.1:3306/javabook";
+		String username = "root";
+		String password = "mysql";
+		
+		// connect to the database
+		try {
+			connection = DriverManager.getConnection(url, username, password);
+			System.out.println("Database connected:");
+			labelStatus.setText("Database connected");
+			statement = connection.createStatement();
+		} catch (SQLException ex) {
+			System.out.println(ex.getMessage());
+			labelStatus.setText("Connection failed");
+		} 
 	}
 
 }
