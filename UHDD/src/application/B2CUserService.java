14518fb027486fbfa3855848a700f7b7baa31e53
TitanSri <104621169+TitanSri@users.noreply.github.com>
2023-07-12 15:44:05 +1000

    Added B2C user login
    This will login to Azure Ad and return the token information.
    Tested fine

--- UHDD/src/application/B2CUserService.java
+++ UHDD/src/application/B2CUserService.java
@@ -15,7 +15,7 @@ import org.springframework.stereotype.Service;
 public class B2CUserService {
 	// the following details is from the app registration page from Azure AD B2C
     private static String clientId = "93622b73-f524-4e0a-abd5-c484ed4cf9bf";
-    private static String clientSecret = "3rM8Q~t3Qz3MEYZJZdIsfYXetGfTqEIy3aiLCaav"; // this is the value not the ID
+    private static String clientSecret = "Z1a8Q~LuPsWAkbs0r1L-ajhC-h_2zEdewDyDJblF"; // this is the value not the ID
     private static String tenantId = "d57abc1a-51fe-44f9-919f-e8f214330064";
     private static String scope = "https://graph.microsoft.com/.default"; // this is for any tenant 
     
@@ -34,13 +34,13 @@ public class B2CUserService {
 
             User user = new User();
             user.accountEnabled = true;
-            user.displayName = "John Doe";
-            user.mailNickname = "JD";
-            user.userPrincipalName = "JohnDoe@uhdbitc309.onmicrosoft.com"; // this needs to be @uhdbitc309.onmicrosoft.com
+            user.displayName = "Jim";
+            user.mailNickname = "JA";
+            user.userPrincipalName = "Jim@uhdbitc309.onmicrosoft.com"; // this needs to be @uhdbitc309.onmicrosoft.com
             user.passwordPolicies = "DisablePasswordExpiration"; // optional
             PasswordProfile passwordProfile = new PasswordProfile();
-            passwordProfile.forceChangePasswordNextSignIn = true; // false if the user does not need to change password
-            passwordProfile.password = "xWwvJ]6NMw+bWH-d";
+            passwordProfile.forceChangePasswordNextSignIn = false; // false if the user does not need to change password
+            passwordProfile.password = "xWwvJ]6NMw+bWH-d"; // the user needs to login to the azure portal and change the password first https://azure.microsoft.com/en-us/get-started/azure-portal
             user.passwordProfile = passwordProfile;
             
             graphClient.users()
