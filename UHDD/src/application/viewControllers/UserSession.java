5fd2a76dc97e1409dcf756c7bd8b88aa3d11bd81
Jono Mason <jonoleo@gmail.com>
2023-07-24 22:46:07 +1000

    Added 2FA for password expiry

--- UHDD/src/application/viewControllers/UserSession.java
+++ UHDD/src/application/viewControllers/UserSession.java
@@ -5,18 +5,23 @@ public class UserSession {
     private static UserSession instance;
 
     private String userName;
+    private String email; // added email field
 
-    private UserSession(String userName) {
+    private UserSession(String userName, String email) { // added email parameter
         this.userName = userName;
+        this.email = email;
     }
 
-    public static void initInstance(String userName) {
+    public static void initInstance(String userName, String email) { // added email parameter
         if(instance == null) {
-            instance = new UserSession(userName);
+            instance = new UserSession(userName, email);
         }
     }
 
     public static UserSession getInstance() {
+        if (instance == null) {
+            throw new IllegalStateException("UserSession has not been initialized.");
+        }
         return instance;
     }
 
@@ -24,11 +29,14 @@ public class UserSession {
         return userName;
     }
 
+    public String getEmail() { // added getEmail method
+        return email;
+    }
+
     public void cleanUserSession() {
-        userName = "";// or null
+        userName = ""; // or null
+        email = ""; // or null
         instance = null;
     }
 
 }
-
-
