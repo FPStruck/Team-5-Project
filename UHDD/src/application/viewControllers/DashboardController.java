cd1394f60563be3b8d590ca17edb738bdaa3d557
Jono Mason <jonoleo@gmail.com>
2023-07-30 17:14:07 +1000

    Updated Patients notes to details

--- UHDD/src/application/viewControllers/DashboardController.java
+++ UHDD/src/application/viewControllers/DashboardController.java
@@ -355,7 +355,7 @@ public class DashboardController {
 	
 	@FXML	
 	public void switchToPatientInfoView(MouseEvent mouseEvent) throws IOException {
-		Parent root = FXMLLoader.load(getClass().getResource("../fxmlScenes/PatientInfoView.fxml"));
+		Parent root = FXMLLoader.load(getClass().getResource("../fxmlScenes/PatientInfoViewOverview.fxml"));
 		stage = (Stage)((Node)mouseEvent.getSource()).getScene().getWindow();
 		scene = new Scene(root);
 		stage.setScene(scene);
