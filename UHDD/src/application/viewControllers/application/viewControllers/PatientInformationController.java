c0cdca85cef7d8e72627b845cdd70468abc08ae7
FPStruck <mina.gemian79@gmail.com>
2023-08-20 20:39:18 +1000

    Main Merge, Implemented encryption methods
    Added previous encryption methods to suit the new version of the main file. Not fully tested in all scenarios. REFER BACK INCASE OF ISSUES

--- UHDD/src/application/viewControllers/application/viewControllers/PatientInformationController.java
+++ UHDD/src/application/viewControllers/application/viewControllers/PatientInformationController.java
@@ -1,7 +1,7 @@
 package application.viewControllers;
 
 import application.DBConnector;
-import application.DataEncryptor;
+import application.DataEncryptorDecryptor;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -95,24 +95,24 @@ public class PatientInformationController {
     private void loadFields(ResultSet results) throws Exception {
         if (results.next()) {
             String encryptionKey = results.getString(21);
-            textLastName.setText(DataEncryptor.decrypt(results.getString(2), encryptionKey));
-            textFirstName.setText(DataEncryptor.decrypt(results.getString(3), encryptionKey));
-            textMiddleName.setText(DataEncryptor.decrypt(results.getString(4), encryptionKey));
-            textAddress.setText(DataEncryptor.decrypt(results.getString(5), encryptionKey));
-            textCity.setText(DataEncryptor.decrypt(results.getString(6), encryptionKey));
-            textState.setText(DataEncryptor.decrypt(results.getString(7), encryptionKey));
-            textTelephone.setText(DataEncryptor.decrypt(results.getString(8), encryptionKey));
-            textEmail.setText(DataEncryptor.decrypt(results.getString(9), encryptionKey));
-            textHealthInsuranceNumber.setText(DataEncryptor.decrypt(results.getString(10), encryptionKey));
-            textEmergencyContactNumber.setText(DataEncryptor.decrypt(results.getString(11), encryptionKey));
-            textAllergies.setText(DataEncryptor.decrypt(results.getString(12), encryptionKey));
-            textPastMedicalConditions.setText(DataEncryptor.decrypt(results.getString(13), encryptionKey));
-            textSurgeriesOrProcedures.setText(DataEncryptor.decrypt(results.getString(14), encryptionKey));
-            textMedications.setText(DataEncryptor.decrypt(results.getString(15), encryptionKey));
-            textImmunisations.setText(DataEncryptor.decrypt(results.getString(16), encryptionKey));
-            textDetails.setText(DataEncryptor.decrypt(results.getString(17), encryptionKey));
-            textFamilyMedicalHistory.setText(DataEncryptor.decrypt(results.getString(18), encryptionKey));
-            textProgressNotes.setText(DataEncryptor.decrypt(results.getString(19), encryptionKey));
+            textLastName.setText(DataEncryptorDecryptor.decrypt(results.getString(2), encryptionKey));
+            textFirstName.setText(DataEncryptorDecryptor.decrypt(results.getString(3), encryptionKey));
+            textMiddleName.setText(DataEncryptorDecryptor.decrypt(results.getString(4), encryptionKey));
+            textAddress.setText(DataEncryptorDecryptor.decrypt(results.getString(5), encryptionKey));
+            textCity.setText(DataEncryptorDecryptor.decrypt(results.getString(6), encryptionKey));
+            textState.setText(DataEncryptorDecryptor.decrypt(results.getString(7), encryptionKey));
+            textTelephone.setText(DataEncryptorDecryptor.decrypt(results.getString(8), encryptionKey));
+            textEmail.setText(DataEncryptorDecryptor.decrypt(results.getString(9), encryptionKey));
+            textHealthInsuranceNumber.setText(DataEncryptorDecryptor.decrypt(results.getString(10), encryptionKey));
+            textEmergencyContactNumber.setText(DataEncryptorDecryptor.decrypt(results.getString(11), encryptionKey));
+            textAllergies.setText(DataEncryptorDecryptor.decrypt(results.getString(12), encryptionKey));
+            textPastMedicalConditions.setText(DataEncryptorDecryptor.decrypt(results.getString(13), encryptionKey));
+            textSurgeriesOrProcedures.setText(DataEncryptorDecryptor.decrypt(results.getString(14), encryptionKey));
+            textMedications.setText(DataEncryptorDecryptor.decrypt(results.getString(15), encryptionKey));
+            textImmunisations.setText(DataEncryptorDecryptor.decrypt(results.getString(16), encryptionKey));
+            textDetails.setText(DataEncryptorDecryptor.decrypt(results.getString(17), encryptionKey));
+            textFamilyMedicalHistory.setText(DataEncryptorDecryptor.decrypt(results.getString(18), encryptionKey));
+            textProgressNotes.setText(DataEncryptorDecryptor.decrypt(results.getString(19), encryptionKey));
             datePicker.setPromptText(results.getString(20));
             labelStatus.setText("Record found");
 
@@ -146,7 +146,7 @@ public class PatientInformationController {
     private void insert(ActionEvent event) throws Exception {
         dbConnection.initialiseDB();
 
-        String encryptionKey = DataEncryptor.generateAESKey();
+        String encryptionKey = DataEncryptorDecryptor.generateAESKey();
 
         String insertQuery1 = "INSERT INTO `testdb`.`test3` (ID, FirstName, MiddleName, LastName, Address, City, "
                 + "State, Telephone, Email, HealthInsuranceNumber, EmergencyContactNumber, Allergies, "
@@ -155,24 +155,24 @@ public class PatientInformationController {
         PreparedStatement statement = dbConnection.connection.prepareStatement(insertQuery1);
         statement.setString(1, textId.getText().trim());
 
-        statement.setString(2, DataEncryptor.encrypt(textFirstName.getText().trim(), encryptionKey));
-        statement.setString(3, DataEncryptor.encrypt(textMiddleName.getText().trim(), encryptionKey));
-        statement.setString(4, DataEncryptor.encrypt(textLastName.getText().trim(), encryptionKey));
-        statement.setString(5, DataEncryptor.encrypt(textAddress.getText().trim(), encryptionKey));
-        statement.setString(6, DataEncryptor.encrypt(textCity.getText().trim(), encryptionKey));
-        statement.setString(7, DataEncryptor.encrypt(textState.getText().trim(), encryptionKey));
-        statement.setString(8, DataEncryptor.encrypt(textTelephone.getText().trim(), encryptionKey));
-        statement.setString(9, DataEncryptor.encrypt(textEmail.getText().trim(), encryptionKey));
-        statement.setString(10, DataEncryptor.encrypt(textHealthInsuranceNumber.getText().trim(), encryptionKey));
-        statement.setString(11, DataEncryptor.encrypt(textEmergencyContactNumber.getText().trim(), encryptionKey));
-        statement.setString(12, DataEncryptor.encrypt(textAllergies.getText().trim(), encryptionKey));
-        statement.setString(13, DataEncryptor.encrypt(textPastMedicalConditions.getText().trim(), encryptionKey));
-        statement.setString(14, DataEncryptor.encrypt(textSurgeriesOrProcedures.getText().trim(), encryptionKey));
-        statement.setString(15, DataEncryptor.encrypt(textMedications.getText().trim(), encryptionKey));
-        statement.setString(16, DataEncryptor.encrypt(textImmunisations.getText().trim(), encryptionKey));
-        statement.setString(17, DataEncryptor.encrypt(textDetails.getText().trim(), encryptionKey));
-        statement.setString(18, DataEncryptor.encrypt(textFamilyMedicalHistory.getText().trim(), encryptionKey));
-        statement.setString(19, DataEncryptor.encrypt(textProgressNotes.getText().trim(), encryptionKey));
+        statement.setString(2, DataEncryptorDecryptor.encrypt(textFirstName.getText().trim(), encryptionKey));
+        statement.setString(3, DataEncryptorDecryptor.encrypt(textMiddleName.getText().trim(), encryptionKey));
+        statement.setString(4, DataEncryptorDecryptor.encrypt(textLastName.getText().trim(), encryptionKey));
+        statement.setString(5, DataEncryptorDecryptor.encrypt(textAddress.getText().trim(), encryptionKey));
+        statement.setString(6, DataEncryptorDecryptor.encrypt(textCity.getText().trim(), encryptionKey));
+        statement.setString(7, DataEncryptorDecryptor.encrypt(textState.getText().trim(), encryptionKey));
+        statement.setString(8, DataEncryptorDecryptor.encrypt(textTelephone.getText().trim(), encryptionKey));
+        statement.setString(9, DataEncryptorDecryptor.encrypt(textEmail.getText().trim(), encryptionKey));
+        statement.setString(10, DataEncryptorDecryptor.encrypt(textHealthInsuranceNumber.getText().trim(), encryptionKey));
+        statement.setString(11, DataEncryptorDecryptor.encrypt(textEmergencyContactNumber.getText().trim(), encryptionKey));
+        statement.setString(12, DataEncryptorDecryptor.encrypt(textAllergies.getText().trim(), encryptionKey));
+        statement.setString(13, DataEncryptorDecryptor.encrypt(textPastMedicalConditions.getText().trim(), encryptionKey));
+        statement.setString(14, DataEncryptorDecryptor.encrypt(textSurgeriesOrProcedures.getText().trim(), encryptionKey));
+        statement.setString(15, DataEncryptorDecryptor.encrypt(textMedications.getText().trim(), encryptionKey));
+        statement.setString(16, DataEncryptorDecryptor.encrypt(textImmunisations.getText().trim(), encryptionKey));
+        statement.setString(17, DataEncryptorDecryptor.encrypt(textDetails.getText().trim(), encryptionKey));
+        statement.setString(18, DataEncryptorDecryptor.encrypt(textFamilyMedicalHistory.getText().trim(), encryptionKey));
+        statement.setString(19, DataEncryptorDecryptor.encrypt(textProgressNotes.getText().trim(), encryptionKey));
         statement.setString(20, datePicker.getValue().toString());
         statement.setString(21, encryptionKey);
 
@@ -217,30 +217,30 @@ public class PatientInformationController {
     @FXML
     private void update(ActionEvent event) throws Exception {
         dbConnection.initialiseDB();
-        String newEncryptionKey = DataEncryptor.generateAESKey();
+        String newEncryptionKey = DataEncryptorDecryptor.generateAESKey();
 
         String updateQuery = "UPDATE `testdb`.`test3` SET FirstName = ?, MiddleName = ?, LastName = ?, Address = ?, City = ?, State = ?, Telephone = ?, Email = ?, "
                 + "HealthInsuranceNumber = ?, EmergencyContactNumber = ?, Allergies = ?, PastMedicalConditions = ?, SurgeriesOrProcedures = ?, Medications = ?, Immunisations = ?,"
                 + "Details =?, FamilyMedicalHistory = ?, ProgressNotes = ?, DateOfBirth = ?, EncryptionKey = ? WHERE ID = ?";
         PreparedStatement statement = dbConnection.connection.prepareStatement(updateQuery);
-        statement.setString(1, DataEncryptor.encrypt(textFirstName.getText().trim(), newEncryptionKey));
-        statement.setString(2, DataEncryptor.encrypt(textMiddleName.getText().trim(), newEncryptionKey));
-        statement.setString(3, DataEncryptor.encrypt(textLastName.getText().trim(), newEncryptionKey));
-        statement.setString(4, DataEncryptor.encrypt(textAddress.getText().trim(), newEncryptionKey));
-        statement.setString(5, DataEncryptor.encrypt(textCity.getText().trim(), newEncryptionKey));
-        statement.setString(6, DataEncryptor.encrypt(textState.getText().trim(), newEncryptionKey));
-        statement.setString(7, DataEncryptor.encrypt(textTelephone.getText().trim(), newEncryptionKey));
-        statement.setString(8, DataEncryptor.encrypt(textEmail.getText().trim(), newEncryptionKey));
-        statement.setString(9, DataEncryptor.encrypt(textHealthInsuranceNumber.getText().trim(), newEncryptionKey));
-        statement.setString(10, DataEncryptor.encrypt(textEmergencyContactNumber.getText().trim(), newEncryptionKey));
-        statement.setString(11, DataEncryptor.encrypt(textAllergies.getText().trim(), newEncryptionKey));
-        statement.setString(12, DataEncryptor.encrypt(textPastMedicalConditions.getText().trim(), newEncryptionKey));
-        statement.setString(13, DataEncryptor.encrypt(textSurgeriesOrProcedures.getText().trim(), newEncryptionKey));
-        statement.setString(14, DataEncryptor.encrypt(textMedications.getText().trim(), newEncryptionKey));
-        statement.setString(15, DataEncryptor.encrypt(textImmunisations.getText().trim(), newEncryptionKey));
-        statement.setString(16, DataEncryptor.encrypt(textDetails.getText().trim(), newEncryptionKey));
-        statement.setString(17, DataEncryptor.encrypt(textFamilyMedicalHistory.getText().trim(), newEncryptionKey));
-        statement.setString(18, DataEncryptor.encrypt(textProgressNotes.getText().trim(), newEncryptionKey));
+        statement.setString(1, DataEncryptorDecryptor.encrypt(textFirstName.getText().trim(), newEncryptionKey));
+        statement.setString(2, DataEncryptorDecryptor.encrypt(textMiddleName.getText().trim(), newEncryptionKey));
+        statement.setString(3, DataEncryptorDecryptor.encrypt(textLastName.getText().trim(), newEncryptionKey));
+        statement.setString(4, DataEncryptorDecryptor.encrypt(textAddress.getText().trim(), newEncryptionKey));
+        statement.setString(5, DataEncryptorDecryptor.encrypt(textCity.getText().trim(), newEncryptionKey));
+        statement.setString(6, DataEncryptorDecryptor.encrypt(textState.getText().trim(), newEncryptionKey));
+        statement.setString(7, DataEncryptorDecryptor.encrypt(textTelephone.getText().trim(), newEncryptionKey));
+        statement.setString(8, DataEncryptorDecryptor.encrypt(textEmail.getText().trim(), newEncryptionKey));
+        statement.setString(9, DataEncryptorDecryptor.encrypt(textHealthInsuranceNumber.getText().trim(), newEncryptionKey));
+        statement.setString(10, DataEncryptorDecryptor.encrypt(textEmergencyContactNumber.getText().trim(), newEncryptionKey));
+        statement.setString(11, DataEncryptorDecryptor.encrypt(textAllergies.getText().trim(), newEncryptionKey));
+        statement.setString(12, DataEncryptorDecryptor.encrypt(textPastMedicalConditions.getText().trim(), newEncryptionKey));
+        statement.setString(13, DataEncryptorDecryptor.encrypt(textSurgeriesOrProcedures.getText().trim(), newEncryptionKey));
+        statement.setString(14, DataEncryptorDecryptor.encrypt(textMedications.getText().trim(), newEncryptionKey));
+        statement.setString(15, DataEncryptorDecryptor.encrypt(textImmunisations.getText().trim(), newEncryptionKey));
+        statement.setString(16, DataEncryptorDecryptor.encrypt(textDetails.getText().trim(), newEncryptionKey));
+        statement.setString(17, DataEncryptorDecryptor.encrypt(textFamilyMedicalHistory.getText().trim(), newEncryptionKey));
+        statement.setString(18, DataEncryptorDecryptor.encrypt(textProgressNotes.getText().trim(), newEncryptionKey));
         statement.setString(19, datePicker.getValue().toString());
         statement.setString(20, newEncryptionKey);
         statement.setString(21, textId.getText().trim());
