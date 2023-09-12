fefddbe1ae356da557b52e810e137e72fe1bd7e5
TitanSri <104621169+TitanSri@users.noreply.github.com>
2023-09-12 10:10:40 +1000

    Clean the code for the application folder and added description for data encryption decryptor class
    Removed empty lines and commented statements
    description for the data encryptor added

--- UHDD/src/application/UI.java
+++ UHDD/src/application/UI.java
@@ -257,10 +257,6 @@ public class UI {
 
 	    //email properties
 	    Properties properties = System.getProperties();  
-//	    properties.setProperty("mail.smtp.host", host);
-//	    properties.setProperty("mail.smtp.port", String.valueOf(port));
-//	    properties.setProperty("mail.smtp.starttls.enable", "true");
-//	    properties.setProperty("mail.smtp.auth", "true");
 	    
 	    // toby's email properties
 	    properties.put("mail.smtp.host", "smtp.gmail.com");
@@ -694,42 +690,30 @@ public class UI {
 
   
   	//Overview button actions
-	
     @FXML
     private void fetchDiagnoses() {
         System.out.println("Hopefully gets 'diagnoses' info");
     }
     
     // Other controller methods and variables
-    
-	
-	
 	@FXML
 	private void fetchHistory() {
 	    System.out.println("Get patient history");
 	}
 	
 	// Other controller methods and variables
-	
-	
-	
 	@FXML
 	private void fetchPerscription() {
 	    System.out.println("Get Patient perscription");
 	}
 	
 	// Other controller methods and variables
-	
-	
-	
 	@FXML
 	private void fetchReport() {
 	    System.out.println("Gets report");
 	}
 	
 	// Other controller methods and variables
-	
-	
 	private void loadFields(ResultSet results) throws SQLException {
 		
 		if (results.next()) {
